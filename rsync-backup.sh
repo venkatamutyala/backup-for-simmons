d99ff1eda3c3ed4f60757c76e0807220e303ea34
Venkata Mutyala <venkata@venkatamutyala.com>
2024-04-29 13:09:42 -0700

    Update rsync-backup.sh

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,1 +1,37 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -e
+
+# Define the mount points and network shares
+MOUNT1="/mnt/qnap/"
+SHARE1="//192.168.6.161/PlexData"
+MOUNT2="/mnt/plexserver/"
+SHARE2="//192.168.4.38/PlexData2"
+
+# Function to check and mount if not already mounted using findmnt
+mount_if_needed() {
+    local share=$1
+    local mountpoint=$2
+
+    # Ensure the mount point directory exists
+    if [ ! -d "$mountpoint" ]; then
+        echo "Creating mount point $mountpoint"
+        mkdir -p "$mountpoint"
+    fi
+
+    # Use findmnt to check if the mount point is already used
+    if findmnt -rno TARGET "$mountpoint" > /dev/null; then
+        echo "$mountpoint is already mounted."
+    else
+        echo "Attempting to mount $share to $mountpoint"
+        mount.cifs "$share" "$mountpoint" -o user=$SYNC_USERNAME,password=$SYNC_PASSWORD,vers=2.1
+        if [ $? -ne 0 ]; then
+            echo "Failed to mount $share on $mountpoint"
+            dmesg | tail -10  # Display the last 10 kernel log messages to help diagnose the issue
+        fi
+    fi
+}
+
+# Mount the shares to the specified mount points
+mount_if_needed "$SHARE1" "$MOUNT1"
+mount_if_needed "$SHARE2" "$MOUNT2"
