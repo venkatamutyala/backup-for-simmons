a889106f398e0bfcb91de13d18d6e17570fc4ee9
rsimmon1 <168495240+rsimmon1@users.noreply.github.com>
2024-05-03 15:30:33 -0700

    Update rsync-backup.sh
    reinstate all rsync's

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -55,17 +55,17 @@ while true; do
     echo "START TIME: ${START_TIME}"
     
     # the folders stored on PlexData2 Share
-    # rsync -avvh --delete "/mnt/qnap/Exercise/" "/mnt/plexserver2/Exercise/"
-    # rsync -avvh --delete "/mnt/qnap/Greg Towes Healing with Oils/" "/mnt/plexserver2/Greg Towes Healing with Oils/"
-    # rsync -avvh --delete "/mnt/qnap/Miscellaneous/" "/mnt/plexserver2/Miscellaneous/"
-    # rsync -avvh --delete "/mnt/qnap/Photos/" "/mnt/plexserver2/Photos/"
-    # rsync -avvh --delete "/mnt/qnap/Robert's Edits/" "/mnt/plexserver2/Robert's Edits/"
+    rsync -avvh --delete "/mnt/qnap/Exercise/" "/mnt/plexserver2/Exercise/"
+    rsync -avvh --delete "/mnt/qnap/Greg Towes Healing with Oils/" "/mnt/plexserver2/Greg Towes Healing with Oils/"
+    rsync -avvh --delete "/mnt/qnap/Miscellaneous/" "/mnt/plexserver2/Miscellaneous/"
+    rsync -avvh --delete "/mnt/qnap/Photos/" "/mnt/plexserver2/Photos/"
+    rsync -avvh --delete "/mnt/qnap/Robert's Edits/" "/mnt/plexserver2/Robert's Edits/"
 
     # # folder stored on PlexData Share
-    #rsync -avvh --delete "/mnt/qnap/backup/" "/mnt/plexserver/backup/"
-    # rsync -avvh --delete "/mnt/qnap/Movies/" "/mnt/plexserver/Movies/"
-    # rsync -avvh --delete "/mnt/qnap/TV Shows/" "/mnt/plexserver/TV Shows/"
-    # rsync -avvh --delete "/mnt/qnap/Vision Boards/" "/mnt/plexserver/Vision Boards/"
+    rsync -avvh --delete "/mnt/qnap/backup/" "/mnt/plexserver/backup/"
+    rsync -avvh --delete "/mnt/qnap/Movies/" "/mnt/plexserver/Movies/"
+    rsync -avvh --delete "/mnt/qnap/TV Shows/" "/mnt/plexserver/TV Shows/"
+    rsync -avvh --delete "/mnt/qnap/Vision Boards/" "/mnt/plexserver/Vision Boards/"
 
     # # folder stored on iTunes Share
     rsync -avvh --delete "/mnt/qnap/iTunes/iTunes Media/" "/mnt/itunes/iTunes Media/"
